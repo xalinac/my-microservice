696f181ff3d6469928e949c9d822ed3ca3a58b4c
Savelii <savakh81@gmail.com>
2024-11-03 01:52:05 +0300

    First commit

---
+++ src/main/java/io/github/xalinac/my_microservice/MyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.xalinac.my_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/xalinac/my_microservice/MyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xalinac.my_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
